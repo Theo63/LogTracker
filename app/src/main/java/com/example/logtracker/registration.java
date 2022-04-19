7566f15ad3f1bcf8dbcefb9ebb59ae83c789528b
Theo63 <tpapadim@csd.auth.gr>
2022-04-19 16:54:18 +0300

    aircraft type spinner added

--- app/src/main/java/com/example/logtracker/registration.java
+++ app/src/main/java/com/example/logtracker/registration.java
@@ -5,26 +5,33 @@ import androidx.fragment.app.DialogFragment;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class registration extends AppCompatActivity {
+public class registration extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 public static Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
         btn = (Button) findViewById(R.id.datePicker);
+
+        Spinner spinner = (Spinner) findViewById(R.id.type_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.Aircraft_types, android.R.layout.simple_spinner_item); //Aircraft_types is in res/values/strings.xml
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
     }
-
-
-
-
-
-
-
-
 
 
 
@@ -36,4 +43,22 @@ public static Button btn;
     public static void setButton(ArrayList<Integer> dates){
         btn.setText(String.valueOf(dates.get(0))+" / "+dates.get(1)+" / "+dates.get(2));
     }
+
+
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+        String aircraft_type = parent.getItemAtPosition(pos).toString();
+        //just a toast to make sure item is passed
+        Toast.makeText(parent.getContext(), aircraft_type, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+
+
+
 }
